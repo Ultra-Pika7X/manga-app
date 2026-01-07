547a697825945f768d0f5b0b04119fe456b3cde5
Ultra-Pika7X <armorwg@gmail.com>
2026-01-07 22:12:12 +0100

    fix: remove seanime folder completely to fix duplicate identifier error

--- seanime-main (2)/seanime-main/internal/manga/providers/proxy_images.go
+++
@@ -1,8 +0,0 @@
-package manga_providers
-
-import util "seanime/internal/util/proxies"
-
-func GetImageByProxy(url string, headers map[string]string) ([]byte, error) {
-	ip := &util.ImageProxy{}
-	return ip.GetImage(url, headers)
-}
